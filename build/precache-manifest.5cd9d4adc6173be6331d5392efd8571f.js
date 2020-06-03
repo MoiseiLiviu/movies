036fb9999b61e19e8ce6dff6827c2ded96012647
Liviu <lmoisei@list.ru>
2020-06-03 14:22:58 +0300

    adding build folder

---
+++ build/precache-manifest.5cd9d4adc6173be6331d5392efd8571f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf4e194ad1e61f9ccf7026b24cdbeaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9c20906e741c36fbf56",
+    "url": "/static/css/2.f93178fb.chunk.css"
+  },
+  {
+    "revision": "40901b9ee4a238d59d7f",
+    "url": "/static/css/main.f822f9af.chunk.css"
+  },
+  {
+    "revision": "d9c20906e741c36fbf56",
+    "url": "/static/js/2.eb8be2f0.chunk.js"
+  },
+  {
+    "revision": "85d965a067415930481a1b5c455d0527",
+    "url": "/static/js/2.eb8be2f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40901b9ee4a238d59d7f",
+    "url": "/static/js/main.e63d855f.chunk.js"
+  },
+  {
+    "revision": "2c1c21c3c7f951dbfb3f",
+    "url": "/static/js/runtime-main.c14b83d2.js"
+  },
+  {
+    "revision": "17fda9667fe2078090e3e5f15bfb8b27",
+    "url": "/static/media/logo2.17fda966.png"
+  }
+]);
